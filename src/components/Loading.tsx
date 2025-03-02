baa7dd1dfd19c31aaf032e631e9d8e80679f37e8
elattachiyoussef <youssefelattachi30900@gmail.com>
2025-03-02 12:02:55 +0100

    Add files via upload

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import React from 'react'
 
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary-600"></div>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-600" />
     </div>
   )
 } 
